4f820ecf3c65deb28cb4b992039535ab9c32b714
wobin1 <nathanielmusa@gmail.com>
2025-01-31 10:05:46 +0100

    changed base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://127.0.0.1:8000/api/'
-  // baseUrl: 'https://starters.hordun.software/'
-
+  // baseUrl: 'http://127.0.0.1:8000/api/'
+  baseUrl: 'http://41.73.6.101:8070/api/'
 };
